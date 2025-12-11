3673a73ecb1d599074a668f401e8b6da917979ac
Hiam <hiam.jh.97@gmail.com>
2025-12-11 23:17:58 +0600

    Update vite.config.ts
    Signed-off-by: Hiam <hiam.jh.97@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { reactRouter } from "@react-router/dev/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
+});
+
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
